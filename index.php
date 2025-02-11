d169327a36aa70eee6fb1a19a19f6aaa3e6f97f7
helmut4646 <elmudfile@gmail.com>
2025-02-11 21:18:56 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,5 +10,9 @@
 	<p>
 		Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex omnis a similique earum voluptates ipsam, aliquam nam numquam id sequi ipsa necessitatibus, minus accusantium in. Optio error fugiat accusamus, laudantium.
 	</p>
+	<ul>
+		<li>satu</li>
+		<li>dua</li>
+	</ul>
 </body>
 </html>
